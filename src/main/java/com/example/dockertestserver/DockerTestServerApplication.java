2c6271fba52f156706d49aa7d53a2dd62b4f5911
Healsu <timmie2500@gmail.com>
2022-09-06 10:45:26 +0200

    Initial commit

---
+++ src/main/java/com/example/dockertestserver/DockerTestServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.dockertestserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerTestServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockerTestServerApplication.class, args);
+    }
+
+}
